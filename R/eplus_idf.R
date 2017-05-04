46a0177515a854b7319b0f3b8044c4705c74225f
Hongyuan Jia <hongyuanjia@outlook.com>
2017-05-04 20:41:20 +0800

    Add `write_idf` function.

--- R/eplus_idf.R
+++ R/eplus_idf.R
@@ -11,6 +11,60 @@ read_idf <- function(idf_path) {
 }
 # }}}1
 
+# find_object
+# {{{1
+find_object <- function (idf, obj_ptn, ignore_case = TRUE, perl = TRUE, invert = FALSE) {
+    ori_names <- names(idf)
+    names(idf) <- make.unique(names(idf), sep = "_")
+    objs <- grep(x = names(idf), pattern = obj_ptn, ignore_case, perl = perl, invert = invert)
+    if (length(objs) == 0) {
+        stop("Could not find any matched objects.", call. = FALSE)
+    } else {
+        results <- idf[c(objs)]
+    }
+    names(idf) <- ori_names
+    return(results)
+}
+# }}}1
+
+# write_idf
+# {{{1
+write_idf <- function (idf, path) {
+    header <-
+        c("!-Generator IDFEditor 1.48",
+          "!-Option SortedOrder",
+          "!-NOTE: All comments with '!-' are ignored by the IDFEditor and are generated automatically.",
+          "!-      Use '!' comments if they need to be retained when using the IDFEditor.",
+          ""
+          )
+
+    obj_header_info <-
+        map(unique(names(idf)),
+            function (name) {
+                upper_name <- str_to_upper(name)
+                index <- grep(x = names(idf), pattern = paste0("^", name, "$")) %>% .[[1]]
+                data.table(name = upper_name, index = index)
+            }) %>% rbindlist(.)
+
+    idf_lines <- map(seq_along(idf),
+                     function (i) {
+                         lines <- create_idf_object_lines(idf[i])
+                         if (!is.na(match(i, obj_header_info[["index"]]))) {
+                             class <- obj_header_info[index == i, name]
+                             obj_header_str <- paste0( "!-   ===========  ALL OBJECTS IN CLASS: ", class, " ===========")
+                             lines <- c(obj_header_str, lines)
+                         }
+                         return(lines)
+          }) %>% flatten_chr()
+    idf <- c(header, idf_lines)
+    write_lines(idf, path = path)
+}
+# }}}1
+
+######################
+#  helper functions  #
+######################
+
 # read_idf_raw
 # {{{1
 read_idf_raw <- function (idf_path) {
@@ -79,7 +133,7 @@ get_idf_object_fields <- function (object) {
     field <- str_replace_all(field_unit, pattern = "(.*)\\s\\{.+\\}", replacement = "\\1")
     unit <- str_extract_all(field_unit, pattern = "\\{.+\\}")
     unit <- str_replace_all(unit, pattern = "[\\{\\}]", replacement = "")
-    unit <- ifelse(unit == "character(0)", "NULL", unit)
+    unit <- ifelse(unit == "character(0)", NA_character_, unit)
     object <- data.table(value = value, field = field, unit = unit)
     return(object)
 }
@@ -101,18 +155,34 @@ get_idf_object <- function (idf) {
 }
 # }}}1
 
-# find_object
+# create_idf_object_lines
 # {{{1
-find_object <- function (idf, obj_ptn, ignore_case = TRUE, perl = TRUE, invert = FALSE) {
-    ori_names <- names(idf)
-    names(idf) <- make.unique(names(idf), sep = "_")
-    objs <- grep(x = names(idf), pattern = obj_ptn, ignore_case, perl = perl, invert = invert)
-    if (length(objs) == 0) {
-        stop("Could not find any matched objects.", call. = FALSE)
-    } else {
-        results <- idf[c(objs)]
-    }
-    names(idf) <- ori_names
-    return(results)
+create_idf_object_lines <- function (object) {
+    object_name <- names(object)
+    object <- object[[1]]
+    value <- object[["value"]]
+    field <- object[["field"]]
+    unit <- object[["unit"]]
+
+    lines <-
+        map_chr(seq(1:nrow(object)),
+                function (i) {
+                    sep <- if_else(i != nrow(object), ",", ";")
+                    value <- str_pad(paste0("    ", value[i], sep), 29, side = "right")
+                    field <- field[i]
+                    unit <- unit[i]
+                    if (is.na(unit)) {
+                        line <- paste0(value, "!- ", field)
+                    } else {
+                        line <- paste0(value, "!- ", field, "{", unit, "}")
+                    }
+                    # if (i == nrow(object)) {
+                    #     line <- paste0(line, "\n\n")
+                    # }
+                    return(line)
+                })
+    header <- paste0(object_name, ",")
+    object_lines <- c(header, lines)
+    return(object_lines)
 }
 # }}}1
