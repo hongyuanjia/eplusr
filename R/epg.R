7f36e29f2eb68eb74b855783792b320c43b67e58
Hongyuan Jia <hongyuanjia@outlook.com>
2017-08-06 13:29:14 +0800

    Rewrite `write_epg` and `run_epg`.

--- R/epg.R
+++ R/epg.R
@@ -15,23 +15,24 @@ read_epg <- function(epg){
     assertthat::assert_that(is_epg_file(epg), msg = "Invalid epg file.")
 
     sim_info <- readr::read_csv(file = epg, comment = "!",
-                                col_names = c("model", "weather", "result", "run_times"),
-                                col_types = cols(.default = "c", run_times = col_integer()))
+                                col_names = c("model", "weather", "output_name", "run_times"),
+                                col_types = cols(.default = "c", run_times = col_integer()),
+                                trim_ws = TRUE)
 
     sim_info <- dplyr::as_tibble(
-        purrr::map_at(sim_info, c("model", "weather", "result"), file_path)
+        purrr::map_at(sim_info, c("model", "weather", "output_name"), file_path)
     )
-    sim_info <- dplyr::mutate(sim_info, output_dir = dirname(result))
-    sim_info <- dplyr::mutate(sim_info, output_prefix = file_path(result))
+    sim_info <- dplyr::mutate(sim_info, output_dir = file_path(dirname(output_name)))
+    sim_info <- dplyr::mutate(sim_info, output_prefix = file_prefix(output_name))
     sim_info <- dplyr::select(sim_info, dplyr::one_of("model", "weather", "output_dir", "output_prefix", "run_times"))
 
-    class(sim_info) <- c("epg", "eplus_job", class(sim_info))
+    class(sim_info) <- c("epg", "eplusr_job", class(sim_info))
 
     return(sim_info)
 }
 # }}}1
 
-# is_epg{{{1
+# is_epg_file {{{1
 is_epg_file <- function (epg) {
     if (assertthat::is.string(epg)) {
         if (has_epg_ext(epg)) {
@@ -54,56 +55,88 @@ is_epg_file <- function (epg) {
 }
 # }}}1
 
-# run_epg{{{1
-run_epg <- function (epg, n = NULL, eplus_ver = NULL, eplus_dir = NULL) {
+# validate_job {{{1
+validate_job <- function (job, type = c("epg", "jeplus", "epat")) {
+    assertthat::assert_that(inherits(job, "eplusr_job"),
+        msg = "Input is not an eplusr job object."
+    )
+    type <- rlang::arg_match(type)
+    val_job <- switch(type,
+        epg = validate_epg(epg),
+        jeplus = validate_jeplus(job),
+        epat = validate_epat(job)
+    )
 
-    # Check 'epg'{{{2
-    if (is.character(epg)) {
-        if (!file.exists(epg)) {
-            stop("Input 'epg' file does not exists.", call. = FALSE)
-        } else {
-            job <- read_epg(epg)
-        }
-    } else if (is.data.frame(epg)) {
-        type <- attr(epg, "job_type")
-        col_names <- colnames(epg)
-        required <- c("model", "weather", "output_dir", "output_prefix", "run_times")
-        ex_col <- setdiff(col_names, required)
-        mis_col <- setdiff(required, col_names)
-        if (all(type == "epg", length(ex_col) == 0L, length(mis_col) == 0L)) {
+    return(val_job)
+}
+
+validate_epg <- function (epg) {
+    if (is_epg_file(epg)) {
+        job <- read_epg(epg)
+    } else {
+        if (inherits(epg, "epg")) {
             job <- epg
         } else {
-            stop("Invalid 'epg'. Please use 'read_epg' to get the correct type", call. = FALSE)
+            stop("'epg' should be an 'epg' object or a file path of an .epg file.",
+                 call. = FALSE)
         }
-    } else {
-        stop("'epg' should be either a data.frame or an .epg file path.", call. = FALSE)
+    }
+
+    return(job)
+}
+# }}}1
+
+# as_epg {{{1
+as_epg <- function (x) {
+    assertthat::assert_that(is.data.frame(x))
+    assertthat::assert_that(identical(ncol(x), 4L))
+
+    col_names = c("model", "weather", "output_name", "run_times")
+    assertthat::assert_that(all(!is.na(match(colnames(x), colnames))),
+        msg = paste0("Input shoud be a data.frame with columns ",
+            sQuote(col_names), "."
+        )
+    )
+
+    epg <- x
+    class(epg) <- unique(c("epg", "eplusr_job", class(sim_info)))
+
+    return(epg)
+}
+# }}}1
+
+# run_epg{{{1
+run_epg <- function (epg, cores = NULL, output_suffix = c("C", "L", "D"),
+                     special_run = NULL, eplus_ver = NULL, eplus_dir = NULL) {
+
+    job <- validate_job(epg, "epg")
+    # Get inputs {{{2
+    models <- job[["model"]]
+    weathers <- job[["weather"]]
+    output_dirs <- job[["output_dir"]]
+    output_prefixes <- job[["output_prefix"]]
+    # }}}2
+    # Write the epg file to output dir {{{2
+    if (identical(length(unique(output_dirs)), 1L)) {
+        write_epg(epg, file_path(output_dirs[1], "run.epg"))
     }
     # }}}2
-
-    # Write epg file{{{2
-    if (unique(job[["output_dir"]]) == 1L) {
-        path <- unique(job[["output_dir"]])
-        if (!dir.exists(path)) {
-            dir.create(path, showWarnings = FALSE, recursive = TRUE)
-        }
-        file_name <- normalizePath(file_path(path, "run.epg"), mustWork = FALSE)
-        write_epg(models = normalizePath(job[["model"]], mustWork = TRUE),
-                  weathers = normalizePath(job[["weather"]], mustWork = TRUE),
-                  output_dirs = normalizePath(file_path(job[["output_dir"]], job[["output_prefix"]])), mustWork = FALSE,
-                  run_times = job[["run_times"]], path = file_name)
-    }
+    # Run {{{2
+    run_multi(models = models, weathers = weathers, cores = cores,
+              output_dirs = output_dirs, output_prefixes = output_prefixes,
+              output_suffix = output_suffix, special_run = special_run,
+              eplus_ver = eplus_ver, eplus_dir = eplus_dir, show_msg = TRUE)
     # }}}2
 
-    # Run
-    rum_multi(models = epg[["model"]], weathers = epg[["weather"]],
-              output_dirs = epg[["output_dir"]], output_prefix = epg[["outout_prefixes"]],
-              n = n, eplus_ver = eplus_ver, eplus_dir = eplus_dir)
+
 }
 # }}}1
 
 # write_epg{{{1
-write_epg <- function(models, weathers, output_dirs, run_times, path){
-    header <- paste0(
+write_epg <- function(epg, path, head_info = NULL){
+    assertthat::assert_that(inherits(epg, "epg"))
+
+    header <- paste(
         "! EnergyPlus Group File",
         "! ------------------------------------------------------------------------------------------------",
         "! Each line represents a specific simulation. If you don't want a simulation to run, add a comment",
@@ -113,10 +146,43 @@ write_epg <- function(models, weathers, output_dirs, run_times, path){
         "!    input file name, weather file name, output file name (no extension), counter",
         "!",
         "! ------------------------------------------------------------------------------------------------",
-        collapse = "\n")
+        sep = "\n", collapse = "\n")
 
-    contents <- paste(models, weathers, output_dirs, run_times, sep = ",")
-    epg_info <- paste(header, contents)
+    epg_content <- dplyr::mutate(epg,
+        output_name = file_path(output_dir, output_prefix),
+        output_dir = NULL, output_prefix = NULL
+    )
+    epg_content <- dplyr::select(epg_content,
+        model, weather, output_name, run_times
+    )
+    # Add 6 leading padding spaces to 'run_times' to create the same format as
+    # original EnergyPlus.
+    epg_content <- dplyr::mutate(epg_content,
+        run_times = stringr::str_pad(as.character(run_times), 7L, "left")
+    )
+
+    content <- readr::format_csv(epg_content, col_names = FALSE)
+    epg_info <- paste(header, content, sep = "\n", collapse = "\n")
+
+    if (!is.null(head_info)) {
+        assertthat::assert_that(is.character(head_info))
+        head_info <- paste0("! ", head_info, collapse = "\n")
+        extra_head <- paste(
+            paste0("! Generated by eplusr ", packageVersion("eplusr")),
+            "! ================================================================================================",
+            "!",
+            head_info,
+            "!",
+            "! ================================================================================================",
+            "!",
+            sep = "\n", collapse = "\n"
+        )
+
+        epg_info <- paste(extra_head, epg_info, "\n", sep = "\n", collapse = "\n")
+    }
+
     readr::write_lines(epg_info, path)
+
+    return(invisible())
 }
 # }}}1
