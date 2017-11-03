a4acc08e723f19e38406611969e3483fa1cfb8bf
Hongyuan Jia <hongyuanjia@outlook.com>
2017-11-03 17:01:16 +0800

    Fix download error.

--- R/wthr_download.R
+++ R/wthr_download.R
@@ -1,6 +1,32 @@
 page_main <- "https://www.energyplus.net"
 
-wthr_download <- function(search, savedir = getwd(), type = c("all", "ddy", "epw", "stat")) {
+#' @title Download weather file from EnergyPlus website
+#' @description Weather data for more than 2100 locations are now available in
+#' EnergyPlus weather format — 1042 locations in the USA, 71 locations in
+#' Canada, and more than 1000 locations in 100 other countries throughout the
+#' world. The weather data are arranged by World Meteorological Organization
+#' region and Country. \code{wthr_download} will search the weather database in
+#' EnergyPlus website, return the results and automatically download the weather
+#' file.
+#' @param search A string used to search in the weather database.
+#' @param savedir Directory where the weather file will be downloaded to. Default: getwd()
+#' @param type The desired file format. Should be one of "all", "ddy", "epw", or
+#' "stat".
+#' @param quiet Whether to show the progress bar of downloading. Default: TRUE
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'     wthr_download("chongqing")
+#'  }
+#' }
+#' @export
+#' @importFrom utils menu
+#' @importFrom rlang arg_match
+#' @importFrom dplyr mutate select filter
+#' @importFrom purrr map pwalk
+#' @importFrom tidyr unnest
+wthr_download <- function(search, savedir = getwd(),
+                          type = c("all", "ddy", "epw", "stat"), quiet = TRUE) {
     src <- wthr_search(search = search)
     cat("Search completed.", nrow(src), "results returned:\n",
         "Please type the number of results you want to download:\n")
@@ -29,11 +55,26 @@ wthr_download <- function(search, savedir = getwd(), type = c("all", "ddy", "epw
         return(invisible())
     }
 
-    purrr::pwalk(dl_tbl, wthr_download_sgl, dir = savedir)
+    purrr::pwalk(dl_tbl, wthr_download_sgl, dir = savedir, quiet = quiet)
 
 }
 
-wthr_search <- function(search = NULL) {
+#' @title Search the weather file database in EnergyPlus.net
+#' @description \code{wthr_search} serach the weather file database in
+#' EnergyPlus website and return the returned inquired results.
+#' @param search A string used to search in the weather database.
+#' @return A data frame contains the returned weather source names and locations.
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  wthr_search("Chongqing")
+#'  }
+#' }
+#' @export
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_node html_children html_text html_attr
+#' @importFrom dplyr tibble
+wthr_search <- function(search) {
     page_wthrsearch <- paste0(page_main, "/weather-search")
 
     results_src_raw <- xml2::read_html(paste0(page_wthrsearch, "/", search)) %>%
@@ -67,15 +108,15 @@ wthr_get_dl_info <- function(path) {
     return(results_dl)
 }
 
-wthr_download_sgl <- function(station, type, url, filename, dir = getwd()) {
-    cat("Start downloading weather source", sQuote(station), "for type",
+wthr_download_sgl <- function(station, type, url, filename, dir = getwd(), quiet = TRUE) {
+    cat("\nStart downloading weather source", sQuote(station), "for type",
         sQuote(type), "...\n\n")
 
     filepath <- file.path(dir, filename)
 
     tryCatch(
         # Have to set mode to 'wb', othewise the downloaded file will be not readable.
-        download.file(url, filepath, mode = "wb", quite = TRUE),
+        download.file(url, filepath, mode = "wb", quiet = quiet),
         error = function(c) paste0("Failed to downloaded file: \n", url)
     )
     cat("File downloaded sucessfully, located at:\n", filepath, "\n")
