7519778e901ad303bbb0a211e3dd126e5dfc42d3
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2017-12-18 02:42:33 +0800

    Seperate macro dict in `is_imf` into a standlone vector.

--- R/idf.R
+++ R/idf.R
@@ -882,24 +882,25 @@ get_idf_ver <- function (idf_str) {
     return(ver)
 }
 # }}}1
+# macro_dict {{{
+macro_dict <-
+      # Incorporating external files
+    c("##include", "##fileprefix", "##includesilent", "##nosilent",
+      # Selective accepting or skipping lins
+      "##if", "##ifdef", "##ifndef", "##elseif", "##else", "##endif",
+      # Defining blocks of input
+      "##def", "##enddef", "##def1", "##set1",
+      # Arithmetic operations
+      "#eval", "#\\[",
+      # Marco debugging and listing
+      "##list", "##nolist", "##show", "##noshow", "##showdetail", "##noshowdetail",
+      # Marco debugging and listing
+      "##expandcomment", "##traceback", "##notraceback", "##write", "##nowrite",
+      # Marco debugging and listing
+      "##symboltable", "##clear", "##reverse", "##!")
+# }}}
 # is_imf {{{1
 is_imf <- function (idf_lines) {
-    macro_dict <-
-          # Incorporating external files
-        c("##include", "##fileprefix", "##includesilent", "##nosilent",
-          # Selective accepting or skipping lins
-          "##if", "##ifdef", "##ifndef", "##elseif", "##else", "##endif",
-          # Defining blocks of input
-          "##def", "##enddef", "##def1", "##set1",
-          # Arithmetic operations
-          "#eval", "#\\[",
-          # Marco debugging and listing
-          "##list", "##nolist", "##show", "##noshow", "##showdetail", "##noshowdetail",
-          # Marco debugging and listing
-          "##expandcomment", "##traceback", "##notraceback", "##write", "##nowrite",
-          # Marco debugging and listing
-          "##symboltable", "##clear", "##reverse", "##!")
-
     is_imf <- any(purrr::map_lgl(macro_dict, ~any(startsWith(idf_lines, .x))))
 
     return(is_imf)
