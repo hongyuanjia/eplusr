55f014045a0efed3c508e3d4fd74dc4bdc707b3a
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2017-12-23 23:49:14 +0800

    Add 'reset' method.

--- R/eplus_model.R
+++ R/eplus_model.R
@@ -18,6 +18,7 @@ eplus_model <- R6::R6Class(classname = "Energy+Model",
             private$idd <- get_idd(self$ver, idd)
             self$model <- parse_idf(private$str, idd = private$idd)
             self$type <- class(self$model)[1]
+            private$time_read <- Sys.time()
         },
 
         list = function (type = c("id", "class", "field"), class = NULL)
@@ -41,16 +42,21 @@ eplus_model <- R6::R6Class(classname = "Energy+Model",
         del = function (id, echo = TRUE)
             idel_object(self, private, id, echo),
 
-        save = function(path, format = c("asis", "sorted", "ori_bot", "ori_top"))
+        save = function (path, format = c("asis", "sorted", "ori_bot", "ori_top"))
             isave_idf(self, path = path, format = format),
 
-        print = function()
-            iprint_idf(self)
+        print = function ()
+            iprint_idf(self),
+
+        reset = function (comfirm = FALSE)
+            ireset_model(self, private, comfirm)
     ),
 
     private = list(
         str = NULL,
-        idd = NULL)
+        idd = NULL,
+        time_read = NULL
+    )
 )
 
 # iprint_idf {{{
@@ -139,6 +145,22 @@ idel_object <- function (self, private, id, echo = TRUE) {
 # iset_object {{{
 iset_object <- function (self, private, id, ..., echo = TRUE) {
     self$model <- set_object(self$model, id, ..., idd = private$idd, verbose = echo)
+
+    return(self)
+}
+# }}}
+
+# ireset_model {{{
+ireset_model <- function (self, private, comfirm = FALSE) {
+    if (!comfirm) {
+        stop(glue::glue("Reset the model back to the status when it was first \\
+            read at {private$time_read}. You will lose all modifications after \\
+            that time and resetting cannot be undone. Comfirm by setting \\
+            'comfirm' to TRUE."), call. = FALSE)
+    }
+    self$ver <- get_idf_ver(private$str)
+    self$model <- parse_idf(private$str, idd = private$idd)
+    self$type <- class(self$model)[1]
 
     return(self)
 }
