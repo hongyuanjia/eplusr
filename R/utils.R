d61104eb0748437f74fc04f8a7540423672bee22
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2017-12-26 16:47:26 +0800

    Fix error in `avail_cols` again.

--- R/utils.R
+++ R/utils.R
@@ -27,7 +27,7 @@ char_count <- function (x, pattern, ...) {
 # }}}
 # avail_cols {{{
 avail_cols <- function (x, table) {
-    names(x)[!is.na(match(names(x), table))]
+    names(x)[match(table, names(x))]
 }
 # }}}
 # sep_line {{{
