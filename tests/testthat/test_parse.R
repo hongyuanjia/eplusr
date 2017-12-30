616de1c20b54ee97e617b6ec17931a46dfd74f9a
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2017-12-31 00:28:11 +0800

    Update test files.

--- tests/testthat/test_parse.R
+++ tests/testthat/test_parse.R
@@ -2,12 +2,12 @@ context("Parse method")
 
 test_that("Parse IDF works", {
 
-    expect_error(model <- eplus_model$new("files/5Zone_Transformer_8.8_bad.idf"))
-    expect_silent(model <- eplus_model$new("files/5Zone_Transformer_8.8.idf"))
-    expect_silent(model <- eplus_model$new("files/5Zone_Transformer_8.7.idf"))
-    expect_silent(model <- eplus_model$new("files/AbsorptionChiller_Macro_8.6.imf"))
+    expect_error(model <- eplus_model$new("5Zone_Transformer_8.8_bad.idf"))
+    expect_silent(model <- eplus_model$new("5Zone_Transformer_8.8.idf"))
+    expect_silent(model <- eplus_model$new("5Zone_Transformer_8.7.idf"))
+    expect_silent(model <- eplus_model$new("AbsorptionChiller_Macro_8.6.imf"))
 
-    model <- eplus_model$new("files/AbsorptionChiller_Macro_8.6.imf")
+    model <- eplus_model$new("AbsorptionChiller_Macro_8.6.imf")
     expect_equal(class(.get(model, "model")), c("IMF", "list"))
     expect_equal(.get(model, "ver"), "8.6")
     expect_equal(.get(model, "type"), "IMF")
