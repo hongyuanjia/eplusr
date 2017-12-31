ecf2c21337fcfb4c6948c51d1ac97a4c22c93282
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2017-12-31 16:16:53 +0800

    Move test files into "inst/testdata".

--- tests/testthat/test_parse.R
+++ tests/testthat/test_parse.R
@@ -2,12 +2,12 @@ context("Parse method")
 
 test_that("Parse IDF works", {
 
-    expect_error(model <- eplus_model$new("5Zone_Transformer_8.8_bad.idf"))
-    expect_silent(model <- eplus_model$new("5Zone_Transformer_8.8.idf"))
-    expect_silent(model <- eplus_model$new("5Zone_Transformer_8.7.idf"))
-    expect_silent(model <- eplus_model$new("AbsorptionChiller_Macro_8.6.imf"))
+    expect_error(model <- eplus_model$new(system.file("testdata", "5Zone_Transformer_8.8_bad.idf", package = "eplusr")))
+    expect_silent(model <- eplus_model$new(system.file("testdata", "5Zone_Transformer_8.8.idf", package = "eplusr")))
+    expect_silent(model <- eplus_model$new(system.file("testdata", "5Zone_Transformer_8.7.idf", package = "eplusr")))
+    expect_silent(model <- eplus_model$new(system.file("testdata", "AbsorptionChiller_Macro_8.6.imf", package = "eplusr")))
 
-    model <- eplus_model$new("AbsorptionChiller_Macro_8.6.imf")
+    model <- eplus_model$new(system.file("testdata", "AbsorptionChiller_Macro_8.6.imf", package = "eplusr"))
     expect_equal(class(.get(model, "model")), c("IMF", "list"))
     expect_equal(.get(model, "ver"), "8.6")
     expect_equal(.get(model, "type"), "IMF")
