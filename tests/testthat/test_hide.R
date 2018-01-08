d6f7206cb570f01dddcd2f58c8f83a00dd8ce7b1
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2018-01-08 10:13:29 +0800

    Fix all errors on Windows and Update README.

--- tests/testthat/test_hide.R
+++ tests/testthat/test_hide.R
@@ -1,20 +1,21 @@
-context("Del method")
+context("Hide method")
 
-test_that("Del method works", {
+test_that("Hide method works", {
     model <- eplus_model$new("files/5Zone_Transformer_8.8.idf")
     id_max <- max_id(.get(model, "model"))
     # missing id
-    expect_error(model$del())
+    expect_error(model$hide())
     # invalid id
-    expect_error(model$del(1e6))
+    expect_error(model$hide(1e6))
     # cannot delete existing unique or required objects
-    expect_error(model$del(1))
-    expect_error(model$del(2))
+    expect_error(model$hide(1))
+    expect_error(model$hide(2))
     # cannot delete objects that have been referred by other objects
-    expect_error(model$del(55))
+    expect_error(model$hide(55))
     expect_equal(.get(model, "model")$class[object_id == 55L, unique(edited)], 0L)
 
     # CORRECT
-    expect_silent(model$del(54))
-    expect_equal(.get(model, "model")$class[object_id == 54L, unique(edited)], -2L)
+    expect_silent(model$hide(54))
+    expect_equal(.get(model, "model")$class[object_id == 54L, unique(edited)], -1L)
 })
+
