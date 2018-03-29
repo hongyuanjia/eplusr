7aba24c2750c3b11945e37894833d984ec07eef7
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2018-03-29 09:23:28 +0800

    Modify idd tests accordingly.

--- tests/testthat/test_idd.R
+++ tests/testthat/test_idd.R
@@ -47,7 +47,7 @@ describe("parse_idd()", {
 
     it("can parse IDD from string", {
         expect_silent(idd_parsed <- parse_idd(idd_normal))
-        expect_equal(names(idd_parsed), c("version", "build", "data",
+        expect_equal(names(idd_parsed), c("version", "build", "order", "data",
                                           "reference", "external"))
     })
 
@@ -68,127 +68,136 @@ describe("parse_idd()", {
         expect_equal(idd_parsed$data$format, c("standard", "singleLine"))
         expect_equal(idd_parsed$data$memo, c(NA_character_, "This is just a test"))
         expect_equal(idd_parsed$data$extensible, c(0, 4))
-        expect_equal(idd_parsed$data$reference_class_name, list(NA_character_, "RefTestSlash"))
+        expect_equal(idd_parsed$data$reference, list(NA_character_, "RefTestSlash"))
         expect_equal(length(idd_parsed$data$data_class), 2)
         expect_equal(class(idd_parsed$data$data_class), "list")
         expect_is(idd_parsed$data$data_class[[1]], "data.table")
-        expect_equal(idd_parsed$data$data_class[[1]],
-                     data.table::data.table(
-                         group_order = 1L,
-                         group = "TestGroup1",
-                         class_order = 1L,
-                         class = "TestSimple",
-                         format = "standard",
-                         memo = NA_character_,
-                         min_fields = 0L,
-                         num_fields = 1L,
-                         required_object = FALSE,
-                         unique_object = FALSE,
-                         extensible = 0L,
-                         reference_class_name = list(NA_character_)
-                    )
+        expect_equivalent(idd_parsed$data$data_class[[1]],
+            data.table::data.table(
+                group_order = 1L,
+                group = "TestGroup1",
+                class_order = 1L,
+                class = "TestSimple",
+                format = "standard",
+                memo = NA_character_,
+                min_fields = 0L,
+                num_fields = 1L,
+                required_object = FALSE,
+                unique_object = FALSE,
+                extensible = 0L,
+                has_reference = FALSE,
+                reference = list(NA_character_)
+            )
         )
-        expect_equal(idd_parsed$data$data_class[[2]],
-                     data.table::data.table(
-                         group_order = 2L,
-                         group = "TestGroup2",
-                         class_order = 2L,
-                         class = "TestSlash",
-                         format = "singleLine",
-                         memo = "This is just a test",
-                         min_fields = 3L,
-                         num_fields = 4L,
-                         required_object = TRUE,
-                         unique_object = TRUE,
-                         extensible = 4L,
-                         reference_class_name = list("RefTestSlash")
-                    )
+        expect_equivalent(idd_parsed$data$data_class[[2]],
+            data.table::data.table(
+                group_order = 2L,
+                group = "TestGroup2",
+                class_order = 2L,
+                class = "TestSlash",
+                format = "singleLine",
+                memo = "This is just a test",
+                min_fields = 3L,
+                num_fields = 4L,
+                required_object = TRUE,
+                unique_object = TRUE,
+                extensible = 4L,
+                has_reference = TRUE,
+                reference = list("RefTestSlash")
+            )
         )
         # }}}
     })
 
     it("can parse field data", {
         # {{{
-        expect_equal(idd_parsed$data$data_field[[1]],
-                     data.table::setnames(data.table::data.table(
-                         class_order = 1L,
-                         class = "TestSimple",
-                         field_order = 1L,
-                         field = "Test Field",
-                         field_anid = "A1",
-                         field_an = "A",
-                         field_id = "1",
-                         units = NA_character_,
-                         ip_units = NA_character_,
-                         required_field = FALSE,
-                         type = NA_character_,
-                         have_range = FALSE,
-                         range = list(NULL),
-                         default = NA_character_,
-                         choice = list(NA_character_),
-                         autosizable = FALSE,
-                         autocalculatable = FALSE,
-                         note = NA_character_,
-                         begin_extensible = FALSE,
-                         reference  = list("RefTestSimpleA1"),
-                         object_list = list(NA_character_),
-                         external_list = list(NA_character_),
-                         unitsbasedonfield = FALSE,
-                         `_field_name` = "Test Field",
-                         `_unit` = "",
-                         `_ip_unit` = "",
-                         `_field` = "Test Field",
-                         `_field_ip` = "Test Field"), "choice", "key"
-                     )
+        expect_equivalent(idd_parsed$data$data_field[[1]],
+            data.table::setnames(data.table::data.table(
+                class_order = 1L,
+                class = "TestSimple",
+                field_order = 1L,
+                field = "Test Field",
+                field_anid = "A1",
+                field_an = "A",
+                field_id = "1",
+                units = NA_character_,
+                ip_units = NA_character_,
+                required_field = FALSE,
+                type = NA_character_,
+                have_range = FALSE,
+                range = list(NULL),
+                default = NA_character_,
+                choice = list(NA_character_),
+                autosizable = FALSE,
+                autocalculatable = FALSE,
+                note = NA_character_,
+                begin_extensible = FALSE,
+                has_reference = TRUE,
+                has_object_list = FALSE,
+                has_external_list = FALSE,
+                unitsbasedonfield = FALSE,
+                `_field_name` = "Test Field",
+                `_unit` = "",
+                `_ip_unit` = "",
+                `_field` = "Test Field",
+                `_field_ip` = "Test Field",
+                reference  = list("RefTestSimpleA1"),
+                object_list = list(NA_character_),
+                external_list = list(NA_character_)), "choice", "key"
+            )
         )
 
-        expect_equal(idd_parsed$data$data_field[[2]],
-                     data.table::setnames(data.table::data.table(
-                         class_order = rep(2L, 4L),
-                         class = rep("TestSlash", 4L),
-                         field_order = 1L:4L,
-                         field = c("Test Character Field 1",
-                                   "Test Numeric Field 1",
-                                   "Test Numeric Field 2",
-                                   "Test Character Field 2"),
-                         field_anid = c("A1", "N1", "N2", "A2"),
-                         field_an = c("A", "N", "N", "A"),
-                         field_id = c("1", "1", "2", "2"),
-                         units = c(NA_character_, "m", NA_character_, NA_character_),
-                         ip_units = c(NA_character_, "inch", NA_character_, NA_character_),
-                         required_field = c(TRUE, rep(FALSE, 3L)),
-                         type = c(NA_character_, "integer", "alpha", NA_character_),
-                         have_range = c(FALSE, TRUE, FALSE, FALSE),
-                         range = list(NULL,
-                                      list(lower = 1L, lower_incbounds = TRUE,
-                                           upper = 10, upper_incbounds = FALSE),
-                                      NULL,
-                                      NULL),
-                         default = c(NA_character_, "2", NA_character_, NA_character_),
-                         choice = list(NA_character_, NA_character_, NA_character_, c("Key1", "Key2")),
-                         autosizable = c(FALSE, TRUE, FALSE, FALSE),
-                         autocalculatable = c(FALSE, FALSE, TRUE, FALSE),
-                         note = c("Test Note Parsing", NA_character_, NA_character_, NA_character_),
-                         begin_extensible = c(TRUE, FALSE, FALSE, FALSE),
-                         reference  = list(NA_character_, NA_character_, NA_character_, NA_character_),
-                         object_list = list(NA_character_, NA_character_, NA_character_, "RefTestSimpleA1"),
-                         external_list = list("autoRDDvariable", NA_character_, NA_character_, NA_character_),
-                         unitsbasedonfield = rep(FALSE, 4L),
-                         `_field_name` = c("Test Character Field 1",
-                                           "Test Numeric Field 1",
-                                           "Test Numeric Field 2",
-                                           "Test Character Field 2"),
-                         `_unit` = c("", "{m}", "", ""),
-                         `_ip_unit` = c("", "{inch}", "", ""),
-                         `_field` = c("Test Character Field 1",
-                                      "Test Numeric Field 1 {m}",
-                                      "Test Numeric Field 2",
-                                      "Test Character Field 2"),
-                         `_field_ip` = c("Test Character Field 1",
-                                         "Test Numeric Field 1 {inch}",
-                                         "Test Numeric Field 2",
-                                         "Test Character Field 2")),
-                     "choice", "key")
+        expect_equivalent(idd_parsed$data$data_field[[2]],
+            data.table::setnames(data.table::data.table(
+                class_order = rep(2L, 4L),
+                class = rep("TestSlash", 4L),
+                field_order = 1L:4L,
+                field = c("Test Character Field 1",
+                          "Test Numeric Field 1",
+                          "Test Numeric Field 2",
+                          "Test Character Field 2"),
+                field_anid = c("A1", "N1", "N2", "A2"),
+                field_an = c("A", "N", "N", "A"),
+                field_id = c("1", "1", "2", "2"),
+                units = c(NA_character_, "m", NA_character_, NA_character_),
+                ip_units = c(NA_character_, "inch", NA_character_, NA_character_),
+                required_field = c(TRUE, rep(FALSE, 3L)),
+                type = c(NA_character_, "integer", "alpha", NA_character_),
+                have_range = c(FALSE, TRUE, FALSE, FALSE),
+                range = list(NULL,
+                             list(lower = 1L, lower_incbounds = TRUE,
+                                  upper = 10, upper_incbounds = FALSE),
+                             NULL,
+                             NULL),
+                default = c(NA_character_, "2", NA_character_, NA_character_),
+                choice = list(NA_character_, NA_character_, NA_character_, c("Key1", "Key2")),
+                autosizable = c(FALSE, TRUE, FALSE, FALSE),
+                autocalculatable = c(FALSE, FALSE, TRUE, FALSE),
+                note = c("Test Note Parsing", NA_character_, NA_character_, NA_character_),
+                begin_extensible = c(TRUE, FALSE, FALSE, FALSE),
+                has_reference = c(FALSE, FALSE, FALSE, FALSE),
+                has_object_list = c(FALSE, FALSE, FALSE, TRUE),
+                has_external_list = c(TRUE, FALSE, FALSE, FALSE),
+                unitsbasedonfield = rep(FALSE, 4L),
+                `_field_name` = c("Test Character Field 1",
+                                  "Test Numeric Field 1",
+                                  "Test Numeric Field 2",
+                                  "Test Character Field 2"),
+                `_unit` = c("", "{m}", "", ""),
+                `_ip_unit` = c("", "{inch}", "", ""),
+                `_field` = c("Test Character Field 1",
+                             "Test Numeric Field 1 {m}",
+                             "Test Numeric Field 2",
+                             "Test Character Field 2"),
+                `_field_ip` = c("Test Character Field 1",
+                                "Test Numeric Field 1 {inch}",
+                                "Test Numeric Field 2",
+                                "Test Character Field 2"),
+                reference  = list(NA_character_, NA_character_, NA_character_, NA_character_),
+                object_list = list(NA_character_, NA_character_, NA_character_, "RefTestSimpleA1"),
+                external_list = list("autoRDDvariable", NA_character_, NA_character_, NA_character_)
+                ),
+            "choice", "key")
         )
         # }}}
     })
@@ -357,7 +366,7 @@ describe("$object()", {
     })
     it("can stop when multiple class names are given", {
         expect_error(idd$object(c("TestSimple", "TestSlash")),
-                     "class is not a string (a length one character vector).")
+                     "class is not a string.")
     })
 })
 
@@ -409,7 +418,7 @@ describe("$reference()", {
 
 describe("$external()", {
     it("can return all external data", {
-        expect_equal(idd$reference(), idd_parsed$external)
+        expect_equal(idd$external(), idd_parsed$external)
     })
 })
 
