378618644b2e7792dfb666322271c8732864785f
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2018-03-29 09:39:50 +0800

    Add iddobject tests.

--- tests/testthat/test_idf_object.R
+++ tests/testthat/test_idf_object.R
@@ -1,7 +1,3 @@
-# Test all methods inherited from `IDDObject`, especially when number of fields
-# of `IDFObject` is less than that in `IDDObject`.
-# idfobj$field_name(index = NULL)
-
 context("IDFObject method")
 
 # idf_text {{{
