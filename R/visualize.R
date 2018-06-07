ac0ed54feed7662d8b761937b07da607319c8dd2
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2018-06-07 18:46:50 +0800

    Add `get_date_colname`.

--- R/visualize.R
+++ R/visualize.R
@@ -1,16 +1,40 @@
+#' @importFrom purrr map map_lgl
+# get_date_colname: A helper function to get the column contains date and time in a
+# data.frame. It will return a name vector of column that has a calss of
+# 'POSIXt' or 'Date'. If none is found, it will return a vector with a length of
+# zero.
+# get_date_colname {{{
+get_date_colname <- function(data){
+    assert_that(is.data.frame(data))
+
+    # Get classes for all columns.
+    classes <- purrr::map(data, class)
+    # Find the column with a class of "POSIXt" or "Date".
+    date_classes <- (purrr::map_lgl(classes, function(classes) 'POSIXt' %in% classes)|
+                     purrr::map_lgl(classes, function(classes) 'Date' %in% classes))
+
+    # Find the name of date column.
+    date_col <- names(which(date_classes))
+
+    return(date_col)
+}
+# }}}
+
 #' @importFrom dygraphs dygraph
 #' @export
 # plot_dygraph {{{
 plot_dygraph <- function (dt, cols = NULL, main = NULL, xlab = NULL, ylab = NULL) {
-    if (!is.null(cols)) dt <- dt[, .SD, .SDcols = unique(c("datetime", cols))]
-    dt %>% data.table::as.xts.data.table() %>%
+    date_col <- get_date_colname(dt)
 
-    dygraphs::dygraph(group = "dygraphs", main = main, xlab = xlab, ylab = ylab) %>%
-    dygraphs::dyRangeSelector() %>%
-    dygraphs::dyHighlight(highlightSeriesOpts = list(strokeWidth = 3)) %>%
-    dygraphs::dyOptions(useDataTimezone = TRUE, axisLineWidth = 1.5) %>%
-    dygraphs::dyLegend(width = 400, show = "follow", labelsSeparateLines = TRUE) %>%
-    dygraphs::dyCrosshair(direction = "vertical") %>%
-    dygraphs::dyCSS(system.file("css", "dygraphs.css", package = "eplusr"))
+    if (!is.null(cols)) dt <- dt[, .SD, .SDcols = unique(c(date_col, cols))]
+
+    data.table::as.xts.data.table(dt) %>%
+        dygraphs::dygraph(group = "dygraphs", main = main, xlab = xlab, ylab = ylab) %>%
+        dygraphs::dyRangeSelector() %>%
+        dygraphs::dyHighlight(highlightSeriesOpts = list(strokeWidth = 3)) %>%
+        dygraphs::dyOptions(useDataTimezone = TRUE, axisLineWidth = 1.5) %>%
+        dygraphs::dyLegend(width = 400, show = "follow", labelsSeparateLines = TRUE) %>%
+        dygraphs::dyCrosshair(direction = "vertical") %>%
+        dygraphs::dyCSS(system.file("css", "dygraphs.css", package = "eplusr"))
 }
 # }}}
