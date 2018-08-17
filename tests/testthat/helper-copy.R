896588901efde5f0d5df98324e20ade38ce6a0c4
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2018-08-18 00:18:34 +0800

    Normalize example file paths before testing for `EplusJob`.

--- tests/testthat/helper-copy.R
+++ tests/testthat/helper-copy.R
@@ -6,11 +6,11 @@ copy_example <- function () {
     ddy_name <- "USA_CA_San.Francisco.Intl.AP.724940_TMY3.ddy"
 
     path_example <- file.path(cfg$dir, "ExampleFiles", example_name)
-    path_idf <- file.path(tempdir(), example_name)
+    path_idf <- normalizePath(file.path(tempdir(), example_name), mustWork = FALSE)
     file.copy(path_example, path_idf, overwrite = TRUE)
 
     path_weather <- file.path(cfg$dir, "WeatherData", weather_name)
-    path_epw <- file.path(tempdir(), weather_name)
+    path_epw <- normalizePath(file.path(tempdir(), weather_name), mustWork = FALSE)
     file.copy(path_weather, path_epw, overwrite = TRUE)
 
     list(idf = path_idf, epw = path_epw)
