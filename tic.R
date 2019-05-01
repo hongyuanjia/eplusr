049ffb63ef5b35e2632357cf24c48afe32b8d006
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-05-01 23:19:18 +0800

    Fix Latex CI error on OSX

--- tic.R
+++ tic.R
@@ -7,10 +7,17 @@ build_args <- c("--force")
 ## do not build manual and vignette on appveyor
 if (.Platform$OS.type == "windows") {
     args <- c("--no-manual", "--no-build-vignettes", args)
-    build_args <- c("--no-build-vignettes", build_args)
+    build_args <- c("--no-manual", "--no-build-vignettes", build_args)
 }
 
 do_package_checks(args = args, build_args = build_args)
+
+# fix LaTeX Error: File `inconsolata.sty' not found
+# https://github.com/travis-ci/travis-ci/issues/7875
+if (Sys.getenv("TRAVIS_OS_NAME") == "osx") {
+    get_stage("before_install") %>%
+    add_code_step(system("sudo tlmgr install inconsolata"))
+}
 
 # pkgdown
 if (ci_get_branch() == "master" && Sys.getenv("TRAVIS_OS_NAME") == "linux") {
