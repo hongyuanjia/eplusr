1a0f73a071f582ed6f636197947db382f7cd99e3
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-05-02 10:52:24 +0800

    Fix CI.

--- tic.R
+++ tic.R
@@ -12,6 +12,6 @@ if (.Platform$OS.type == "windows" || Sys.getenv("TRAVIS_OS_NAME") == "osx") arg
 do_package_checks(args = args, build_args = build_args)
 
 # pkgdown
-if (ci_get_branch() == "master" && Sys.getenv("TRAVIS_OS_NAME") == "linux") {
+if (ci_get_branch() == "master" && Sys.getenv("TRAVIS_OS_NAME") == "linux" && Sys.getenv("TRAVIS_R_VERSION_STRING") == "release") {
     do_pkgdown(commit_paths = "docs/*", document = TRUE, path = "docs", branch = "gh-pages")
 }
