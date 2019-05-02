3502b8d7434174ba9f0fa795c6655e082ca8dc79
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2019-05-02 17:04:58 +0800

    Clean tic.R.

--- tic.R
+++ tic.R
@@ -15,13 +15,7 @@ do_package_checks(args = args, build_args = build_args)
 # make sure to clean site to rebuild everything
 if (ci_get_branch() == "master" && Sys.getenv("TRAVIS_OS_NAME") == "linux" && Sys.getenv("TRAVIS_R_VERSION_STRING") == "release") {
     get_stage("before_deploy") %>%
-        add_step(step_setup_ssh()) %>%
-        add_step(step_setup_push_deploy(branch = "gh-pages", orphan = TRUE))
+        add_code_step(pkgdown::clean_site())
 
-    get_stage("deploy") %>%
-        step_run_code(pkgdown::clean_site())
-
-    get_stage("deploy") %>%
-        add_step(step_build_pkgdown(document = TRUE)) %>%
-        add_step(step_do_push_deploy())
+    do_pkgdown(commit_paths = "docs/*", document = TRUE)
 }
