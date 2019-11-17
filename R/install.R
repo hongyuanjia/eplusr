f3e7cd6cc9ea95b7bc8a0a3107fc8f97a2b6d5dc
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-11-17 18:16:24 +0800

    Fix unmount arguments.

--- R/install.R
+++ R/install.R
@@ -324,7 +324,7 @@ install_eplus_macos <- function (exec, local = FALSE) {
     } else {
         system(sprintf("sudo installer -pkg /Volumes/%s/%s.pkg -target LocalSystem", no_ext, no_ext))
     }
-    system(sprintf("hdiutil unmount /Volumes/%s/%s.pkg"))
+    system(sprintf("hdiutil unmount /Volumes/%s/%s.pkg"), no_ext, no_ext)
 }
 # }}}
 # install_eplus_linux {{{
