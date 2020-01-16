c4f11fa2dca2431600d1cb135901b023fbf4129b
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-01-16 13:20:29 +0800

    Fix tests.

--- tests/testthat/test_idd.R
+++ tests/testthat/test_idd.R
@@ -43,7 +43,7 @@ test_that("can read IDD", {
     expect_true(is_avail_idd("9.9.9"))
 
     # can parse old IDD
-    expect_silent(use_idd(8.1, download = "auto"))
+    expect_warning(use_idd(8.1, download = "auto"))
 })
 # }}}
 
