2712aad5664c3ab613fc93b13d443147fb6f739d
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-01-19 03:42:34 +0800

    Do not inherit EplusSql class in EplusJob.

--- R/job.R
+++ R/job.R
@@ -32,7 +32,7 @@ NULL
 
 #' @export
 # EplusJob {{{
-EplusJob <- R6::R6Class(classname = "EplusJob", cloneable = FALSE, inherit = EplusSql,
+EplusJob <- R6::R6Class(classname = "EplusJob", cloneable = FALSE,
     public = list(
 
         # INITIALIZE {{{
@@ -331,6 +331,341 @@ EplusJob <- R6::R6Class(classname = "EplusJob", cloneable = FALSE, inherit = Epl
         #'
         read_mdd = function ()
             job_read_mdd(self, private),
+        # }}}
+
+        # list_table {{{
+        #' @description
+        #' List all table names in EnergyPlus SQL output
+        #'
+        #' @details
+        #' `$list_table()` returns all available table and view names in the
+        #' EnergyPlus SQLite file.
+        #'
+        #' @return A character vector
+        #'
+        #' @examples
+        #' \dontrun{
+        #' job$list_table()
+        #' }
+        #'
+        list_table = function ()
+            job_list_table(self, private),
+        # }}}
+
+        # read_table {{{
+        #' @description
+        #' Read a single table from EnergyPlus SQL output
+        #'
+        #' @details
+        #' `$read_table()` takes a valid table `name` of those from
+        #' \href{../../eplusr/html/EplusJob.html#method-list_table}{\code{$list_table()}}
+        #' and returns that table data in a [data.table::data.table()] format.
+        #'
+        #' @param name A single string specifying the name of table to read.
+        #'
+        #' @return A [data.table::data.table()].
+        #'
+        #' @examples
+        #' \dontrun{
+        #' # read a specific table
+        #' job$read_table("Zones")
+        #' }
+        #'
+        read_table = function (name)
+            job_read_table(self, private, name),
+        # }}}
+
+        # report_data_dict {{{
+        #' @description
+        #' Read report data dictionary from EnergyPlus SQL output
+        #'
+        #' @details
+        #' `$report_data_dict()` returns a [data.table::data.table()] which
+        #' contains all information about report data.
+        #'
+        #' For details on the meaning of each columns, please see "2.20.2.1
+        #' ReportDataDictionary Table" in EnergyPlus "Output Details and
+        #' Examples" documentation.
+        #'
+        #' @return A [data.table::data.table()] of 10 columns:
+        #'
+        #' * `report_data_dictionary_index`: The integer used to link the
+        #'   dictionary data to the variable data. Mainly useful when joining
+        #'   diferent tables
+        #' * `is_meter`: Whether report data is a meter data. Possible values:
+        #'   `0` and `1`
+        #' * `timestep_type`: Type of data timestep. Possible values: `Zone` and
+        #'   `HVAC System`
+        #' * `key_value`: Key name of the data
+        #' * `name`: Actual report data name
+        #' * `reporting_frequency`:
+        #' * `schedule_name`: Name of the the schedule that controls reporting
+        #'     frequency.
+        #' * `units`: The data units
+        #'
+        #' @examples
+        #' \dontrun{
+        #' job$report_data_dict()
+        #' }
+        #'
+        report_data_dict = function ()
+            job_report_data_dict(self, private),
+        # }}}
+
+        # report_data {{{
+        #' @description
+        #' Read report data
+        #'
+        #' @details
+        #' `$report_data()` extracts the report data in a
+        #' [data.table::data.table()] using key values, variable names and other
+        #' specifications.
+        #'
+        #' `$report_data()` can also directly take all or subset output from
+        #' `$report_data_dict()` as input, and extract all data specified.
+        #'
+        #' The returned column numbers varies depending on `all` argument.
+        #'
+        #' * `all` is `FALSE`, the returned [data.table::data.table()] has 6 columns:
+        #'   * `case`: Simulation case specified using `case` argument
+        #'   * `datetime`: The date time of simulation result
+        #'   * `key_value`: Key name of the data
+        #'   * `name`: Actual report data name
+        #'   * `units`: The data units
+        #'   * `value`: The data value
+        #' * `all` is `TRUE`, besides columns described above, extra columns are also
+        #'   included:
+        #'   * `month`: The month of reported date time
+        #'   * `day`: The day of month of reported date time
+        #'   * `hour`: The hour of reported date time
+        #'   * `minute`: The minute of reported date time
+        #'   * `dst`: Daylight saving time indicator. Possible values: `0` and `1`
+        #'   * `interval`: Length of reporting interval
+        #'   * `simulation_days`: Day of simulation
+        #'   * `day_type`: The type of day, e.g. `Monday`, `Tuesday` and etc.
+        #'   * `environment_period_index`: The indice of environment.
+        #'   * `environment_name`: A text string identifying the environment.
+        #'   * `is_meter`: Whether report data is a meter data. Possible values: `0` and
+        #'     `1`
+        #'   * `type`: Nature of data type with respect to state. Possible values: `Sum`
+        #'     and `Avg`
+        #'   * `index_group`: The report group, e.g. `Zone`, `System`
+        #'   * `timestep_type`: Type of data timestep. Possible values: `Zone` and `HVAC
+        #'     System`
+        #'   * `reporting_frequency`: The reporting frequency of the variable, e.g.
+        #'   `HVAC System Timestep`, `Zone Timestep`.
+        #'   * `schedule_name`: Name of the the schedule that controls reporting
+        #'     frequency.
+        #'
+        #' With the `datetime` column, it is quite straightforward to apply time-series
+        #' analysis on the simulation output. However, another painful thing is that
+        #' every simulation run period has its own `Day of Week for Start Day`. Randomly
+        #' setting the `year` may result in a date time series that does not have
+        #' the same start day of week as specified in the RunPeriod objects.
+        #'
+        #' eplusr provides a simple solution for this. By setting `year` to `NULL`,
+        #' which is the default behavior, eplusr will calculate a year value (from
+        #' current year backwards) for each run period that compliances with the start
+        #' day of week restriction.
+        #'
+        #' It is worth noting that EnergyPlus uses 24-hour clock system where 24 is only
+        #' used to denote midnight at the end of a calendar day. In EnergyPlus output,
+        #' "00:24:00" with a time interval being 15 mins represents a time period from
+        #' "00:23:45" to "00:24:00", and similarly "00:15:00" represents a time period
+        #' from "00:24:00" to "00:15:00" of the next day. This means that if current day
+        #' is Friday, day of week rule applied in schedule time period "00:23:45" to
+        #' "00:24:00" (presented as "00:24:00" in the output) is also Friday, but not
+        #' Saturday. However, if you try to get the day of week of time "00:24:00" in R,
+        #' you will get Saturday, but not Friday. This introduces inconsistency and may
+        #' cause problems when doing data analysis considering day of week value.
+        #'
+        #' With `wide` equals `TRUE`, `$report_data()` will format the simulation output
+        #' in the same way as standard EnergyPlus csv output file. Sometimes this can be
+        #' useful as there may be existing tools/workflows that depend on this format.
+        #' When both `wide` and `all` are `TRUE`, columns of runperiod environment names
+        #' and date time components are also returned, including:
+        #' `environment_period_index", "environment_name`, `simulation_days`,
+        #' `datetime`, `month`, `day`, `hour`, `minute`, `day_type`.
+        #'
+        #' For convenience, input character arguments matching in
+        #' `$report_data()` are **case-insensitive**.
+        #'
+        #' @param key_value A character vector to identify key values of the
+        #'        data. If `NULL`, all keys of that variable will be returned.
+        #'        `key_value` can also be data.frame that contains `key_value`
+        #'        and `name` columns. In this case, `name` argument in
+        #'        `$report_data()` is ignored. All available `key_value` for
+        #'        current simulation output can be obtained using
+        #'        \href{../../eplusr/html/EplusSql.html#method-report_data_dict}{\code{$report_data_dict()}}.
+        #'        Default: `NULL`.
+        #'
+        #' @param name A character vector to identify names of the data. If
+        #'        `NULL`, all names of that variable will be returned. If
+        #'        `key_value` is a data.frame, `name` is ignored. All available
+        #'        `name` for current simulation output can be obtained using
+        #'        \href{../../eplusr/html/EplusSql.html#method-report_data_dict}{\code{$report_data_dict()}}.
+        #'        Default: `NULL`.
+        #'
+        #' @param year Year of the date time in column `datetime`. If `NULL`, it
+        #'        will calculate a year value that meets the start day of week
+        #'        restriction for each environment. Default: `NULL`.
+        #'
+        #' @param tz Time zone of date time in column `datetime`. Default:
+        #'        `"UTC"`.
+        #'
+        #' @param case If not `NULL`, a character column will be added indicates
+        #'        the case of this simulation. If `"auto"`, the name of the IDF
+        #'        file without extension is used.
+        #'
+        #' @param all If `TRUE`, extra columns are also included in the returned
+        #'        [data.table::data.table()].
+        #'
+        #' @param wide If `TRUE`, the output is formated in the same way as
+        #'        standard EnergyPlus csv output file.
+        #'
+        #' @param period A Date or POSIXt vector used to specify which time
+        #'        period to return. The year value does not matter and only
+        #'        month, day, hour and minute value will be used when
+        #'        subsetting. If `NULL`, all time period of data is returned.
+        #'        Default: `NULL`.
+        #'
+        #' @param month,day,hour,minute Each is an integer vector for month,
+        #'        day, hour, minute subsetting of `datetime` column when
+        #'        querying on the SQL database. If `NULL`, no subsetting is
+        #'        performed on those components. All possible `month`, `day`,
+        #'        `hour` and `minute` can be obtained using
+        #'        \href{../../eplusr/html/EplusSql.html#method-read_table}{\code{$read_table("Time")}}.
+        #'        Default: `NULL`.
+        #'
+        #' @param interval An integer vector used to specify which interval
+        #'        length of report to extract. If `NULL`, all interval will be
+        #'        used. Default: `NULL`.
+        #'
+        #' @param simulation_days An integer vector to specify which simulation
+        #'        day data to extract. Note that this number resets after warmup
+        #'        and at the beginning of an environment period. All possible
+        #'        `simulation_days` can be obtained using
+        #'        \href{../../eplusr/html/EplusSql.html#method-read_table}{\code{$read_table("Time")}}.
+        #'        If `NULL`, all simulation days will be used. Default: `NULL`.
+        #'
+        #' @param day_type A character vector to specify which day type of data
+        #'        to extract. All possible day types are: `Sunday`, `Monday`,
+        #'        `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`,
+        #'        `Holiday`, `SummerDesignDay`, `WinterDesignDay`, `CustomDay1`,
+        #'        and `CustomDay2`. All possible values for current simulation
+        #'        output can be obtained using
+        #'        \href{../../eplusr/html/EplusSql.html#method-read_table}{\code{$read_table("Time")}}.
+        #'
+        #' @param environment_name A character vector to specify which
+        #'        environment data to extract. If `NULL`, all environment data
+        #'        are returned. Default: `NULL`. All possible
+        #'        `environment_name` for current simulation output can be
+        #'        obtained using:
+        #' ```
+        #' $read_table("EnvironmentPeriods")
+        #' ```
+        #'
+        #' @return A [data.table::data.table()].
+        #'
+        #' @examples
+        #' \dontrun{
+        #' # read all report data
+        #' job$report_data()
+        #'
+        #' # specify output variables using report data dictionary
+        #' dict <- job$report_data_dict()
+        #' job$report_data(dict[units == "C"])
+        #'
+        #' # specify output variables using 'key_value' and 'name'
+        #' job$report_data("environment", "site outdoor air drybulb temperature")
+        #'
+        #' # explicitly specify year value and time zone
+        #' job$report_data(dict[1], year = 2020, tz = "Etc/GMT+8")
+        #'
+        #' # explicitly specify case name
+        #' job$report_data(dict[1], case = "example")
+        #'
+        #' # get all possible columns
+        #' job$report_data(dict[1], all = TRUE)
+        #'
+        #' # return in a format that is similar as EnergyPlus CSV output
+        #' job$report_data(dict[1], wide = TRUE)
+        #'
+        #' # return in a format that is similar as EnergyPlus CSV output with
+        #' # extra columns
+        #' job$report_data(dict[1], wide = TRUE, all = TRUE)
+        #'
+        #' # only get data at the working hour on the first Monday
+        #' job$report_data(dict[1], hour = 8:18, day_type = "monday", simulation_days = 1:7)
+        #'
+        #' # only get specified run period data
+        #' job$read_table("EnvironmentPeriods") # possible environment name
+        #' job$report_data(dict[1], environment_name = "San Francisco Intl Ap CA USA TMY3 WMO#=724940")
+        #' # can also be done using 'environment_period_index' column
+        #' job$report_data(dict[1], all = TRUE)[environment_period_index == 3L]
+        #' }
+        #'
+        report_data = function (key_value = NULL, name = NULL, year = NULL,
+                                tz = "UTC", case = "auto", all = FALSE, wide = FALSE,
+                                period = NULL, month = NULL, day = NULL, hour = NULL, minute = NULL,
+                                interval = NULL, simulation_days = NULL, day_type = NULL,
+                                environment_name = NULL)
+            job_report_data(self, private, key_value = key_value, name = name, year = year,
+                tz = tz, case = case, all = all, wide = wide,
+                period = period, month = month, day = day, hour = hour, minute = minute,
+                interval = interval, simulation_days = simulation_days, day_type = day_type,
+                environment_name = environment_name
+            ),
+        # }}}
+
+        # tabular_data {{{
+        #' @description
+        #' Read tabular data
+        #'
+        #' @details
+        #' `$tabular_data()` extracts the tabular data in a
+        #' [data.table::data.table()] using report, table, column and row name
+        #' specifications. The returned [data.table::data.table()] has
+        #' 8 columns:
+        #'
+        #' * `index`: Tabular data index
+        #' * `report_name`: The name of the report that the record belongs to
+        #' * `report_for`: The `For` text that is associated with the record
+        #' * `table_name`: The name of the table that the record belongs to
+        #' * `column_name`: The name of the column that the record belongs to
+        #' * `row_name`: The name of the row that the record belongs to
+        #' * `units`: The units of the record
+        #' * `value`: The value of the record **in string format**
+        #'
+        #' For convenience, input character arguments matching in
+        #' `$tabular_data()` are **case-insensitive**.
+        #'
+        #' @param report_name,report_for,table_name,column_name,row_name Each is
+        #'        a character vector for subsetting when querying the SQL
+        #'        database.  For the meaning of each argument, please see the
+        #'        description above.
+        #'
+        #' @return A [data.table::data.table()] with 8 columns.
+        #'
+        #' @examples
+        #' \dontrun{
+        #' # read all tabular data
+        #' job$tabular_data()
+        #'
+        #' # explicitly specify data you want
+        #' str(job$tabular_data(
+        #'     report_name = "AnnualBuildingUtilityPerformanceSummary",
+        #'     table_name = "Site and Source Energy",
+        #'     column_name = "Total Energy",
+        #'     row_name = "Total Site Energy"
+        #' ))
+        #' }
+        #'
+        tabular_data = function(report_name = NULL, report_for = NULL, table_name = NULL,
+                                column_name = NULL, row_name = NULL)
+            job_tabular_data(self, private, report_name = report_name,
+                report_for = report_for, table_name = table_name,
+                column_name = column_name, row_name = row_name),
         # }}}
 
         # print {{{
