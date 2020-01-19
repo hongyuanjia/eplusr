c828e25419ee22c798320e933e5b6cb8d58d2e5c
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-01-19 12:26:56 +0800

    Fix inherited method cross-reference links in website.

--- tic.R
+++ tic.R
@@ -14,7 +14,34 @@ do_package_checks(args = args, build_args = build_args, codecov = FALSE)
 # pkgdown
 # make sure to clean site to rebuild everything
 if (ci_get_branch() == "master" && Sys.getenv("TRAVIS_OS_NAME") == "linux" && Sys.getenv("TRAVIS_R_VERSION_STRING") == "release") {
-    do_pkgdown(document = TRUE, orphan = TRUE)
+    get_stage("before_deploy") %>%
+        add_step(step_setup_ssh())
+
+    get_stage("deploy") %>%
+        # a hack before https://github.com/r-lib/pkgdown/issues/1173 is fixed
+        add_code_step({
+            f <- list.files("man", "^[A-Z].*\\.Rd", full.names = TRUE)
+            update_ref <- function (Rd) {
+                if (!length(Rd)) return(NULL)
+                l <- data.table::fread(Rd, sep = NULL, header = FALSE, col.names = "string")
+                ln <- l[stringi::stri_detect_fixed(string, '\\item \\out{<span class="pkg-link" data-pkg="eplusr"'), which = TRUE]
+                if (!length(ln)) return(NULL)
+
+                l[ln, string := stringi::stri_replace_all_fixed(string,
+                    "\\href{../../eplusr/html",
+                    "\\href{../../eplusr/reference",
+                )]
+
+                data.table::fwrite(l, Rd, col.names = FALSE, quote = FALSE)
+            }
+            lapply(f, update_ref)
+        })
+
+    get_stage("deploy") %>%
+        add_step(step_build_pkgdown(document = FALSE))
+
+    get_stage("deploy") %>%
+        add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
 
 # codecov
