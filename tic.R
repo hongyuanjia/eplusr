c6327aabfa305e6f0242f5870aa6053ccf92e81c
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-08-19 00:55:41 +0800

    [CI] Use xvfb for rgl

--- tic.R
+++ tic.R
@@ -1,3 +1,13 @@
+# RGL
+get_stage("before_install") %>%
+    add_code_step(options(rgl.useNULL = TRUE)) %>%
+    add_code_step(system("/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24"))
+
+get_stage("before_script") %>%
+    add_code_step(system("export DISPLAY=:99.0")) %>%
+    add_code_step(system("sh -e /etc/init.d/xvfb start")) %>%
+    add_code_step(system("sleep 3"))
+
 # R CMD Check
 ## OS specific build and check arguments
 ## get as cran from environment
