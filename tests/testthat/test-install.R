cbbd0807d745abf7189ddd17abce4d39bb05bec4
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2022-11-29 19:58:27 +0800

    [test] Use v22.1 for testing

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -6,21 +6,21 @@ test_that("Install", {
     else install_eplus(8.8, local = TRUE)
 
     # test if patch on EnergyPlus v9.1 and above works
-    if (!is_avail_eplus(9.5)) {
-        expect_equivalent(res <- install_eplus(9.5, local = TRUE), 0L)
+    if (!is_avail_eplus(22.1)) {
+        expect_equivalent(res <- install_eplus(22.1, local = TRUE), 0L)
         installer <- attr(res, "installer")
 
         # can update EnergyPlus config
-        expect_true(is_avail_eplus(9.5))
+        expect_true(is_avail_eplus(22.1))
 
         # can uninstall EnergyPlus
-        expect_equivalent(uninstall_eplus(9.5), 0L)
+        expect_equivalent(uninstall_eplus(22.1), 0L)
 
         # can remove EnergyPlus config
-        expect_false("9.5.0" %in% as.character(avail_eplus()))
+        expect_false("22.1.0" %in% as.character(avail_eplus()))
 
-        # still need EnergyPlus v9.5 for testing transitions
-        install_eplus_from_file(9.5, installer, TRUE)
+        # still need EnergyPlus v22.1 for testing transitions
+        install_eplus_from_file(22.1, installer, TRUE)
         # refresh config database
         expect_is(locate_eplus(), "numeric_version")
     }
