f6c1d2691b36b6f20300ecebca4fa58a2f111221
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-08-25 21:25:38 +0800

    [CRAN] Explicitly set DT threads to 2 on CRAN machines

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,10 @@
 .onLoad <- function(libname, pkgname) {
+    # To get rid of the CRAN check NOTE like:
+    # > Running R code in 'testthat.R' had CPU time 6.6 times elapsed time
+    # See: https://github.com/Rdatatable/data.table/issues/5658
+    if (any(grepl("_R_CHECK", names(Sys.getenv()), fixed = TRUE))) {
+        data.table::setDTthreads(2)
+    }
     locate_eplus()
     reg_custom_units()
 }
