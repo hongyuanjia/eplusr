bb63687b00a62a1d681eb764b09749fd3966d219
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2024-02-08 16:31:14 +0800

    test(helper): only auto-install EnergyPlus when on CI

--- tests/testthat/helper-eplus.R
+++ tests/testthat/helper-eplus.R
@@ -1,4 +1,6 @@
-if (identical(Sys.getenv("NOT_CRAN"), "true") && !is_avail_eplus(LATEST_EPLUS_VER)) {
+if (identical(Sys.getenv("NOT_CRAN"), "true") &&
+    identical(Sys.getenv("CI"), "true") &&
+    !is_avail_eplus(LATEST_EPLUS_VER)) {
     install_eplus(LATEST_EPLUS_VER, local = TRUE)
     # NOTE: From EnergyPlus v23.1, the supported oldest version for transition
     # is v9.0. Have to install at least one version before v23.1 for testing
